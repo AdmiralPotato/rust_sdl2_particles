92879aa52599be34aff7c6ec9f539fc0307d0c3b
Admiral Potato <admiral@nuclearpixel.com>
2023-12-17 17:28:01 -0700

    Let's use Nalgebra for Points and Vectors

--- src/particle.rs
+++ src/particle.rs
@@ -1,22 +1,17 @@
-#[derive(Copy, Clone)]
-pub struct Point {
-    pub x: f32,
-    pub y: f32,
-}
+use nalgebra::{Point2, Vector2};
+pub type Point = Point2<f32>;
+pub type Vector = Vector2<f32>;
 
-impl std::ops::Add for Point {
-    type Output = Point;
-    fn add(self, v: Point) -> Point {
-        Point {
-            x: self.x + v.x,
-            y: self.y + v.y,
-        }
-    }
-}
+/*
+Note for future Admiral: If you find yourself needing to turn a Point into a
+Vector, (for instance, when averaging points) use my_point.coords instead of
+my_point.into(). my_point.coords will just give you the coordinates,
+as a vector. .into() will convert it into a *homogenous* vector, `x, y, 1`.
+*/
 
 pub struct Particle {
     pub pos: Point,
-    pub vel: Point,
+    pub vel: Vector,
     pub lifespan: u32,
     pub index: u32,
 }
@@ -24,8 +19,8 @@ pub struct Particle {
 impl Particle {
     pub fn new(x: f32, y: f32, index: u32) -> Particle {
         Particle {
-            pos: Point { x, y },
-            vel: Point { x: 1.0, y: 0.0 },
+            pos: Point::new(x, y),
+            vel: Vector::new(1.0, 0.0),
             lifespan: 160,
             index,
         }
